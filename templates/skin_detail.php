4d7e1754aa7c43a32bbd2405862a7db2c0df63fa
Boy Aditya Rohmaulana <135103722+boyaditya@users.noreply.github.com>
2024-04-27 09:11:11 +0700

    Add files via upload

---
+++ templates/skin_detail.php
@@ -0,0 +1,9 @@
+<?php include __DIR__ . '/header.php'; ?>
+
+<div class="container mt-3">
+
+  DATA_DETAIL
+
+</div>
+
+<?php include __DIR__ . '/footer.php'; ?>
---
+++ templates/skin_form.php
@@ -0,0 +1,10 @@
+<?php include __DIR__ . '/header.php'; ?>
+
+<div class="container mt-3">
+
+  FORM_DATA
+
+</div>
+
+
+<?php include __DIR__ . '/footer.php'; ?>
